11ee84de21e01460c97c33ddf55d3e199755524f
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-09 17:57:57 -0700

    Time: 34 ms (88.09%), Space: 16.4 MB (23.83%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
@@ -7,17 +7,21 @@ class Solution:
 
         if n<2:
             return n
+        elif n==2:
+            return 6
 
-        dp = [ 0 for _ in range(n+1)]
-        dp[1]=1
-        dp[2]=6
+        # dp = [ 0 for _ in range(n+1)]
+        # dp[1]=1
+        # dp[2]=6
+        prev = 6
         mod = 10**9+7
 
         for i in range(3,n+1):
 
             each = 2*i -1
             total = ((each*(each+1))/2)%mod
-            dp[i] = (total*dp[i-1])%mod
+            ans = (total*prev)%mod
+            prev = ans
 
-        return int(dp[n]%mod)
+        return int(ans%mod)
 
