b01315096a1787545b83bc2b15558e6a81be966a
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-09 17:58:24 -0700

    Time: 28 ms (99.15%), Space: 16.2 MB (69.79%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
@@ -3,25 +3,25 @@ import math
 class Solution:
     def countOrders(self, n: int) -> int:
         
-        # return (math.factorial(n*2)//(2**n))%(10**9+7)
+        return (math.factorial(n*2)//(2**n))%(10**9+7)
 
-        if n<2:
-            return n
-        elif n==2:
-            return 6
+        # if n<2:
+        #     return n
+        # elif n==2:
+        #     return 6
 
-        # dp = [ 0 for _ in range(n+1)]
-        # dp[1]=1
-        # dp[2]=6
-        prev = 6
-        mod = 10**9+7
+        # # dp = [ 0 for _ in range(n+1)]
+        # # dp[1]=1
+        # # dp[2]=6
+        # prev = 6
+        # mod = 10**9+7
 
-        for i in range(3,n+1):
+        # for i in range(3,n+1):
 
-            each = 2*i -1
-            total = ((each*(each+1))/2)%mod
-            ans = (total*prev)%mod
-            prev = ans
+        #     each = 2*i -1
+        #     total = ((each*(each+1))/2)%mod
+        #     ans = (total*prev)%mod
+        #     prev = ans
 
-        return int(ans%mod)
+        # return int(ans%mod)
 
