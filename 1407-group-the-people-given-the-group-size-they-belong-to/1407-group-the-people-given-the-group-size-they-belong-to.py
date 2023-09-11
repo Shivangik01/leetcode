57b6401c6ec2dda1a8e9d69af9b94806370e7080
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-11 03:33:15 -0700

    Time: 70 ms (82.92%), Space: 16.6 MB (31.58%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,36 @@
+# sort
+# curr counter and next counter
+
+from collections import defaultdict
+
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+
+        res = []
+        hp = defaultdict(list)
+
+        for i in range(len(groupSizes)):
+            hp[groupSizes[i]].append(i)
+
+        # print(hp)
+
+        groupSizes = sorted(groupSizes)
+        curr = groupSizes[0]
+        temp = []
+
+        for i in range(len(groupSizes)):
+            #print(i,curr)
+
+            if curr!=0:
+                curr -= 1
+                temp.append(hp[groupSizes[i]].pop())
+
+            else:
+                res.append(temp)
+                temp = []
+                curr = groupSizes[i]-1
+                temp.append(hp[groupSizes[i]].pop())
+
+        res.append(temp)
+        
+        return res
