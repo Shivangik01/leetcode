d7fbc03353e36a76e7e171157bf4ab9c04d9cfff
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-11 17:50:46 -0700

    Time: 169 ms (51.47%), Space: 17.1 MB (62.1%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution:
+    def minDeletions(self, s: str) -> int:
+
+        hp = defaultdict(int)
+
+        for a in s:
+            hp[a]+=1
+
+        dels = 0
+        freq = sorted(hp.values(), reverse=True)
+        seen = []
+
+        for val in freq:
+            
+            if val in seen:   
+                
+                while val in seen and val>0:
+                    val -= 1
+                    dels += 1
+            
+            seen.append(val)
+
+        #print(seen,dels)
+        return dels
+
+        
