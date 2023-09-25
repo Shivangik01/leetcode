c4feab1fa2bfe79d7bf2483a47310620ceb2bcc1
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-25 00:17:59 -0700

    Time: 204 ms (73.23%), Space: 17.4 MB (43.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,23 +1,44 @@
 # for 
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+    def findCircleNum(self, M: List[List[int]]) -> int:
 
-        provinces = 0   
-        n = len(isConnected)
-        vis = [0]*n
+        # provinces = 0   
+        # n = len(isConnected)
+        # vis = [0]*n
 
-        def dfs(i):
-            for j in range(n):
-                if isConnected[i][j]==1:
-                    isConnected[i][j]=-1
-                    vis[j]=1
-                    dfs(j)
+        # def dfs(i):
+        #     for j in range(n):
+        #         if isConnected[i][j]==1:
+        #             isConnected[i][j]=-1
+        #             vis[j]=1
+        #             dfs(j)
 
+        # for i in range(n):
+
+        #     if vis[i]==0:
+        #         vis[i]=1
+        #         provinces +=1
+        #         dfs(i)
+
+        # return provinces
+
+        if not M:
+            return 0
+        
+        n = len(M)
+        visit = [False]*n
+        
+        def dfs(u):
+            for v in range(n):
+                if M[u][v] ==1 and visit[v] == False:
+                    visit[v] = True
+                    dfs(v)
+        
+        count = 0
         for i in range(n):
-
-            if vis[i]==0:
-                vis[i]=1
-                provinces +=1
+            if visit[i] == False:
+                count += 1
+                visit[i] = True
                 dfs(i)
-
-        return provinces
+        
+        return count
