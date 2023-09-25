ff2098d805629cf470976370d1d329e6c96ba861
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-25 00:16:16 -0700

    Time: 311 ms (5.03%), Space: 27.2 MB (8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -4,28 +4,20 @@ class Solution:
 
         provinces = 0   
         n = len(isConnected)
+        vis = [0]*n
+
+        def dfs(i):
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    isConnected[i][j]=-1
+                    vis[j]=1
+                    dfs(j)
 
         for i in range(n):
-            for j in range(n):
 
-                if isConnected[i][j]==1:
-
-                    q=[(i,j)]
-                    provinces +=1
-
-                    while q!=[]:
-
-                        a,b = q.pop(0)
-
-                        for k in range(n):
-                            if isConnected[a][k]==1:
-                                isConnected[a][k] = -1
-                                q.append((a,k))
-                        
-                        for k in range(n):
-                            if isConnected[b][k]==1:
-                                isConnected[b][k] = -1
-                                q.append((b,k))
-
+            if vis[i]==0:
+                vis[i]=1
+                provinces +=1
+                dfs(i)
 
         return provinces
