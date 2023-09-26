84195481949ced2dd7b0e2e494e807f28874325d
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-09-26 00:13:16 -0700

    Time: 473 ms (7.62%), Space: 21.2 MB (22.78%) - LeetHub

---
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        if n==1:
+            return nums[0]
+        
+        op = [0 for _ in range(n)]
+        ssop = [i for i in nums]
+        ans1 = nums[0]
+        ss = nums[-1]
+
+        for i in range(n-2,-1,-1):
+            ss += nums[i]
+            ssop[i]= max(ssop[i+1],ss)
+
+        #print(ssop)
+
+        ans2 = nums[0]
+        ps = 0
+
+        for i in range(n):
+            ps += nums[i]
+            op[i]= max(op[i-1]+nums[i],nums[i])
+            ans1 = max(op[i],ans1)
+            ans2 = ans2 if i==n-1 else max(ans2,ps+ssop[i+1])
+        
+
+        return max(ans1,ans2)
