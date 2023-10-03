1ee2baf995670dfa0c1ce1093e51c0f64e98d0fb
Shivangi Kochrekar <49335609+Shivangik01@users.noreply.github.com>
2023-10-02 21:38:17 -0400

    Time: 47 ms (15.52%), Space: 16.1 MB (97.25%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.py
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -2,24 +2,24 @@ from collections import Counter, defaultdict
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
 
-        # mp = Counter(nums)
+        mp = Counter(nums)
 
-        # #print(mp)
-        # res = 0
-
-        # for val in mp.values():
-            
-        #     res += math.comb(val, 2)
-
-        # return res
-
-        mp = defaultdict(int)
+        #print(mp)
         res = 0
 
-        for num in nums:
-
-            res += mp[num]
-            mp[num]+=1
+        for val in mp.values():
+            
+            res += math.comb(val, 2)
 
         return res
+
+        # mp = defaultdict(int)
+        # res = 0
+
+        # for num in nums:
+
+        #     res += mp[num]
+        #     mp[num]+=1
+
+        # return res
         
